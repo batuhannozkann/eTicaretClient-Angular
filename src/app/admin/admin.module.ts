d015c3f15e29a807e05bf64d687890e870b348b6
batuhannozkann <batuhanozkan.cont@gmail.com>
2023-05-25 23:56:06 +0300

    ETicaretClient

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from './layout/layout.module';
+import { ComponentsModule } from './components/components.module';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LayoutModule
+  ],
+  exports:[
+    LayoutModule,
+    ComponentsModule,
+    RouterModule
+  ]
+})
+export class AdminModule { }
---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    CustomersModule,
+    OrdersModule,
+    DashboardModule,
+    RouterModule
+  ]
+})
+export class ComponentsModule { }
